32a5e32d2773e7583b0ad162360794f3a4e9f1d7
Joaquin Jimenez <joaajimenezcav@unal.edu.co>
2019-01-30 17:28:51 +0100

    * Add URL to search page

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
     path('authors/', views.AuthorsListView.as_view(), name='authors'),
     re_path(r'^author/(?P<pk>\d+)$', views.AuthorDetailView.as_view(), name='author-detail'),
-
+    path('search/', views.BookSearch, name='book-search'),
 ]
