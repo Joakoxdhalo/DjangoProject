534e4843bac7ccdb88c2a9d213539ef33062015f
Joaquin Jimenez <joaajimenezcav@unal.edu.co>
2019-02-09 18:46:50 +0100

    * Add personal Key for google Books searches

--- locallibrary/services/services.py
+++ locallibrary/services/services.py
@@ -3,6 +3,8 @@ import requests
 
 def get_books(to_search):
     url = "https://www.googleapis.com/books/v1/volumes?q={}".format(to_search)
+    MyAPIKey = "AIzaSyC11sce0KPIZ6ryLjrCSGDn9zEDSVbK-XM"
+    url = F"{url}&key={MyAPIKey}"
     print(url)
     r = requests.get(url)
     books = r.json()
